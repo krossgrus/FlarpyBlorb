4c9d9dd4cb837b68152288aa0defcc726ea05916
krossgrus <109951198+krossgrus@users.noreply.github.com>
2023-11-13 15:12:25 +0100

    Added pipecheat script (empty)

---
+++ Assets/PipeCheat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeCheat : MonoBehaviour
+{
+    public Collider2D myCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+}
